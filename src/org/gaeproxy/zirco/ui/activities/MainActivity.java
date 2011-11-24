e06b3f28ab0e072d5be96eaeee8bf9abae248caa
max.c.lv <tommy4711@users.noreply.github.com>
2011-11-24 09:07:27 +0800

    deprecated

--- src/org/gaeproxy/zirco/ui/activities/MainActivity.java
+++ src/org/gaeproxy/zirco/ui/activities/MainActivity.java
@@ -1549,7 +1549,6 @@ public class MainActivity extends Activity implements IToolbarsContainer,
 
 	@Override
 	protected void onPause() {
-		WebView.disablePlatformNotifications();
 		mCurrentWebView.doOnPause();
 		super.onPause();
 	}
@@ -1563,7 +1562,6 @@ public class MainActivity extends Activity implements IToolbarsContainer,
 
 	@Override
 	protected void onResume() {
-		WebView.enablePlatformNotifications();
 		mCurrentWebView.doOnResume();
 		super.onResume();
 	}

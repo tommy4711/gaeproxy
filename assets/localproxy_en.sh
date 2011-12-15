0783731bf0179babeb66761eeaad4fb27d7c8c3f
max.c.lv <tommy4711@users.noreply.github.com>
2011-12-15 14:07:11 +0800

    chmod 644

--- assets/localproxy_en.sh
+++ assets/localproxy_en.sh
@@ -5,7 +5,7 @@ export LD_LIBRARY_PATH=/data/data/org.gaeproxy/python/lib
 export PYTHONHOME=$PYTHONHOME:/data/data/org.gaeproxy/python
 export TEMP=$1/python-extras
 
-chmod 755 /data/data/org.gaeproxy/python.pid
+chmod 644 /data/data/org.gaeproxy/python.pid
 kill -9 `cat /data/data/org.gaeproxy/python.pid`
 rm /data/data/org.gaeproxy/python.pid
 killall -9 python
